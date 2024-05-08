2fc43ad1dd3a94b8df335f32eee6ca5d70fb5154
Kiran tej <duggukirantej@gmail.com>
2024-05-08 18:07:05 +0530

    Insurance App UI

---
+++ InsuranceApp/app/src/main/java/com/example/insuranceapp/Clam.java
@@ -0,0 +1,31 @@
+package com.example.insuranceapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Clam extends AppCompatActivity implements View.OnClickListener  {
+
+    //Button butn1,butn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clam);
+
+
+//        butn1 = findViewById(R.id.loginbackbtn);
+//        butn1.setOnClickListener(this);
+//        butn1.setOnClickListener(e ->startActivity(new Intent(clam.this, MainActivity.class)));
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
