b7b62674172e34a44c3d77cdaeb2cf94a9af5be2
Kiran tej <duggukirantej@gmail.com>
2024-05-23 10:02:54 +0530

    UI-design

--- InsuranceApp/app/src/main/java/com/example/insuranceapp/Request.java
+++ InsuranceApp/app/src/main/java/com/example/insuranceapp/Request.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Request extends AppCompatActivity implements View.OnClickListener  {
 
     Button butn1,butn2;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,12 @@ public class Request extends AppCompatActivity implements View.OnClickListener  
         butn1 = findViewById(R.id.contactbtn);
         butn1.setOnClickListener(this);
         butn1.setOnClickListener(e ->startActivity(new Intent(Request.this, Contact.class)));
+
+        butn2 = findViewById(R.id.statusbtn);
+        butn2.setOnClickListener(this);
+        butn2.setOnClickListener(e ->startActivity(new Intent(Request.this, Status.class)));
+
+
 
     }
 
